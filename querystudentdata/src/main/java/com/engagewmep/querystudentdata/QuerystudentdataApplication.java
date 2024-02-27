1ef253a77e7ab8afa1fed1555979f5a622053995
Mahathi <mahathi252001@gmail.com>
2024-02-26 19:09:40 -0500

    Initial commit

---
+++ querystudentdata/src/main/java/com/engagewmep/querystudentdata/QuerystudentdataApplication.java
@@ -0,0 +1,13 @@
+package com.engagewmep.querystudentdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerystudentdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerystudentdataApplication.class, args);
+	}
+
+}
---
+++ querystudentdata/src/test/java/com/engagewmep/querystudentdata/QuerystudentdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engagewmep.querystudentdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerystudentdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
